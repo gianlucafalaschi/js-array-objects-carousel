c19ac536c23135f03d2fa0cc55e978a7b0e3604e
gianlucafalaschi <gian100gian@outlook.it>
2024-03-21 16:46:37 +0100

    feat: images container

--- js/script.js
+++ js/script.js
@@ -1,3 +1,25 @@
+/* 
+Consegna:
+Dato un array di oggetti letterali con:
+ - url dell’immagine
+ - titolo
+ - descrizione
+Creare un carosello come nella foto allegata.
+Milestone 0:
+Come nel primo carosello realizzato, focalizziamoci prima sulla creazione
+ del markup statico: costruiamo il container e inseriamo l'immagine grande in modo da poter stilare lo slider.
+
+ Milestone 1:
+Ora rimuoviamo i contenuti statici e usiamo l’array di oggetti letterali per popolare dinamicamente il carosello.
+Al click dell'utente sulle frecce verso alto o basso, l'immagine attiva diventerà visibile e dovremo aggiungervi titolo e testo.
+
+Milestone 2:
+Aggiungere il **ciclo infinito** del carosello. Ovvero se la miniatura attiva è la prima 
+e l'utente clicca la freccia verso l'alto, la miniatura che deve attivarsi sarà l'ultima e viceversa 
+per l'ultima miniatura se l'utente clicca la freccia verso il basso.
+*/
+
+
 const images = [
     {
         image: 'img/01.webp',
